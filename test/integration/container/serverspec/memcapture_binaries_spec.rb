806d1cb64f5e6f69425aef614ec763cd8dc73648
juju4 <juju4@users.noreply.github.com>
2016-09-11 18:18:19 -0400

    sync repositories + travis test: docker

---
+++ test/integration/container/serverspec/memcapture_binaries_spec.rb
@@ -0,0 +1,22 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe file('/tmp/ir-bin/volatility/vol.py') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+end
+
+describe file('/tmp/ir-bin/linpmem-2.1.post4') do
+  it { should be_file }
+  it { should be_executable }
+  it { should be_owned_by 'root' }
+end
+
+describe file('/tmp/ir-bin/lmg/lmg') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+end
+
+
