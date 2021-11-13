8762f8a39bcccdfe7bd6ceba5efab872a2ca149c
juju4 <juju4@users.noreply.github.com>
2021-11-13 14:22:20 -0500

    test/default: add golang and volatility roles

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,8 @@ rolesdir=$(dirname $0)/..
 fi
 
 #[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
+[ ! -d $rolesdir/juju4.volatility ] && git clone https://github.com/juju4/ansible-volatility $rolesdir/juju4.volatility
+[ ! -d $rolesdir/juju4.golang ] && git clone https://github.com/juju4/ansible-golang $rolesdir/juju4.golang
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.memcapture ] && ln -s ansible-memcapture $rolesdir/juju4.memcapture
 [ ! -e $rolesdir/juju4.memcapture ] && cp -R $rolesdir/ansible-memcapture $rolesdir/juju4.memcapture
